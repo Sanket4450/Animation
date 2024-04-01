f2ff3596cea082c3c692a39d3c6b9d3b33c165ba
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-04-01 12:57:59 +0530

    box1 rotate animation done

--- First/anime1.js
+++ First/anime1.js
@@ -3,6 +3,16 @@ const circle2 = document.querySelector('.circle2')
 
 gsap.to([circle1, circle2], {
     x: 300,
-    duration: 4,
+    duration: 3,
+    ease: 'none',
     backgroundColor: '#f77234',
+})
+
+gsap.to('.box1', {
+    rotation: 360,
+    x: '50vw',
+    duration: 3,
+    repeat: 3,
+    ease: 'none',
+    yoyo: true,
 })
